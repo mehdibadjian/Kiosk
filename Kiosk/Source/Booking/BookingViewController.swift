4be786ad2dec74aa0d001802a7e8a819b853f193
Mehdi <mmbadjian@gmail.com>
2018-09-13 20:04:31 +0800

    Added tabbar controller in storyboard, Added view controllers for Booking and History pages

---
+++ Kiosk/Source/Booking/BookingViewController.swift
@@ -0,0 +1,16 @@
+//
+//  BookingViewController.swift
+//  Kiosk
+//
+//  Created by Mehdi on 13/09/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class BookingViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
---
+++ Kiosk/Source/History/HistoryViewController.swift
@@ -0,0 +1,16 @@
+//
+//  HistoryViewController.swift
+//  Kiosk
+//
+//  Created by Mehdi on 13/09/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class HistoryViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+  }
+}
