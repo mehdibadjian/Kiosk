0f69d3f6db02d76deb45424d969039e3718436d2
Mehdi <mmbadjian@gmail.com>
2018-09-13 17:27:56 +0800

    Initial Commit

---
+++ Kiosk/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kiosk
+//
+//  Created by Mehdi on 13/09/2018.
+//  Copyright © 2018 Mehdi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
